af3013fca9baeeae56ded15c197017cb7ef978ef
aravergar <aravergar@gmail.com>
2018-05-15 13:14:43 +0200

    first commit

---
+++ src/test/java/com/aravergar/lungar/LungarApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aravergar.lungar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LungarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
