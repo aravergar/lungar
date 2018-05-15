4012e72ea3f81a7621535ba70b39f2fcec7e768f
aravergar <aravergar@gmail.com>
2018-05-15 18:10:58 +0200

    javafx and spring integrated

--- src/main/java/com/aravergar/lungar/Controller.java
+++ src/main/java/com/aravergar/lungar/Controller.java
@@ -2,24 +2,33 @@ package com.aravergar.lungar;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 @Component
 public class Controller {
     @Autowired
     private ArticleRepository articleRepository;
 
+    private String wRoot = new String("\u00e6");
+    private Mono<String> wSuffix = Mono.empty();
+
     @FXML
     private Text text;
-    private String ra = new String("\u027b"+"a");
+    @FXML
+    private TextField textField;
+//    private String ra = new String("\u027b"+"a");
+
     public void setText(String value){
         text.textProperty().set(value);
     }
 
     public void doSomething(ActionEvent actionEvent) {
         System.out.println("clicked");
-        setText(ra);
+        wSuffix.subscribe();
+//        wSuffix. textField.getText();
     }
 }
